4acadc7f2e80e808638064f795c15df6f8a7424a
Rubén <ruben.bellido.navarro@gmail.com>
2025-01-22 13:56:38 +0100

    añadidas funciones delBBDDPlayer(), returnListRanking(), BBDDconnect, reports() (todas de Marc).

--- M03-PROGRAMACION/funciones.py
+++ M03-PROGRAMACION/funciones.py
@@ -431,6 +431,199 @@ def removeBBDDPlayer():
 seleccionemos'''
     pass
 
+def delBBDDPlayer(nif):
+    '''Función que elimina un jugador de la BBDD'''
+    cursor=BBDDconnect()
+    cursor.execute(f"SET FOREIGN_KEY_CHECKS = 0;")
+    cursor.execute(f"delete from players where player_id like {nif}")
+    print("Eliminado jugador/a "+ str(nif))
+
+def returnListRanking(field='earnings'):
+    '''Función que retorna una lista ordenada según la opción del ranking elegida'''
+    cursor=BBDDconnect()
+    if field=='earnings':
+        cursor.execute('select * from player_statistics order by earnings desc')
+    elif field=='games':
+        cursor.execute('select * from player_statistics order by games_played desc')
+    elif field=='minutes':
+        cursor.execute('select * from player_statistics order by minutes_played desc')
+    results = cursor.fetchall()
+    table_ranking = ("*" * 60 + "\n" + "Player ID".ljust(12) + "Name".ljust(10) + "Earnings".ljust(10)
+                     + "Games Played".ljust(13) + "Minutes Played".ljust(15) + "\n" + "*" * 60)
+    print(table_ranking)
+    # Filas de datos
+    for row in results:
+        print(
+            str(row[0]).ljust(12) +
+            str(row[1]).ljust(10) +
+            str(row[2]).ljust(10) +
+            str(row[3]).ljust(13) +
+            str(row[4]).ljust(15)
+        )
+
+def BBDDconnect():
+    '''Función que conecta con la base de datos para insertar, borrar y modificar valores.'''
+    conn = pymysql.connect(
+        host='proyectosieteymedio.mysql.database.azure.com',
+        user='adminproyecto',
+        password='proyecto1234!',
+        database='siete_y_medio')
+    cursor = conn.cursor()
+    return cursor
+
+def reports():
+    '''Función que nos muestra el menú de reportes, y una vez elegida una opción, el reporte
+correspondiente'''
+    # pass
+    inputOptReports = (
+        '1)  Initial card more repeated by each user, only users who have played a minimum of 3 games.',
+        '2)  Player who makes the highest bet per game, find the round with the highest bet.',
+        '3)  Player who makes the lowest bet per game.',
+        '4)  Percentage of rounds won per player in each game (%), as well as their average bet for the game.',
+        '5)  List of games won by Bots.',
+        '6)  Number of users have been the bank in each game.',
+        '7)  Rounds won by the bank in each game.',
+        '8)  Average bet per game.',
+        '9)  Average bet of the first round of each game.',
+        '10) Average bet of the last round of each game.',
+        '11) Go back'
+    )
+    validInputReports = []
+    for n in range(1, 12):
+        validInputReports.append(n)
+    userInput = getOpt(strReports, inputOptReports, validInputReports)
+    # mainMenu()
+    # TODO Todas las opciones de la función
+    if userInput == 1:
+        print("En proceso")
+        input("Enter para continuar")
+        reports()
+
+    elif userInput == 2:
+        cabecera = (" APUESTA MÁS ALTA POR PARTIDA ".center(85, "=") + "\n" + "ID Partida".ljust(
+            15) + "ID Jugador".rjust(15) +
+                    "Apuesta Más Alta".rjust(30) + "\n" + "*".center(85, "="))
+
+        cursor = BBDDconnect()
+        cursor.execute("SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));")
+        cursor.execute("select * from informe_2")
+        results = cursor.fetchall()
+        print(cabecera)
+        for row in results:
+            print(str(row[0]).ljust(15) + str(row[1]).rjust(15) + str(row[2]).rjust(30))
+
+        input("Enter para continuar.")
+        reports()
+
+    elif userInput == 3:
+        cabecera = (" APUESTA MÁS BAJA POR PARTIDA ".center(85, "=") + "\n" + "ID Partida".ljust(
+            15) + "ID Jugador".rjust(15) + "Apuesta Más Baja".rjust(30) + "\n" + "*".center(85, "="))
+
+        cursor = BBDDconnect()
+        cursor.execute("SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));")
+        cursor.execute("select * from informe_3")
+        results = cursor.fetchall()
+        print(cabecera)
+        for row in results:
+            print(str(row[0]).ljust(15) + str(row[1]).rjust(15) + str(row[2]).rjust(30))
+
+        input("Enter para continuar.")
+        reports()
+
+    elif userInput == 4:
+        print("En proceso")
+        input("Enter para continuar")
+        reports()
+    elif userInput == 5:
+        print("En proceso")
+        input("Enter para continuar")
+        reports()
+
+    elif userInput == 6:
+
+        cabecera = (" RONDAS GANADAS POR BANCA ".center(60, "=") + "\n" + "ID Partida".ljust(20) +
+                    "Rondas Ganadas".rjust(30) + "\n" + "*".center(60, "="))
+
+        cursor = BBDDconnect()
+        cursor.execute("SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));")
+        cursor.execute("select * from informe_6")
+        results = cursor.fetchall()
+        print(cabecera)
+        for row in results:
+            print(str(row[0]).ljust(20) + str(row[1]).rjust(30))
+
+        input("Enter para continuar")
+        reports()
+
+    elif userInput == 7:
+
+        cabecera = (" USUARIOS BANCAS POR PARTIDA".center(60, "=") + "\n" + "ID Partida".ljust(20) +
+                    "Usuarios Banca".rjust(30) + "\n" + "*".center(60, "="))
+
+        cursor = BBDDconnect()
+        cursor.execute("SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));")
+        cursor.execute("select * from informe_7")
+        results = cursor.fetchall()
+        print(cabecera)
+        for row in results:
+            print(str(row[0]).ljust(20) + str(row[1]).rjust(30))
+
+        input("Enter para continuar")
+        reports()
+
+    elif userInput == 8:
+
+        cabecera = (" APUESTA MEDIA POR PARTIDA ".center(60, "=") + "\n" + "ID Partida".ljust(20) +
+                    "Apuesta Media".rjust(30) + "\n" + "*".center(60, "="))
+
+        cursor = BBDDconnect()
+        cursor.execute("SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));")
+        cursor.execute("select * from informe_8")
+        results = cursor.fetchall()
+        print(cabecera)
+        for row in results:
+            print(str(row[0]).ljust(20) + str(row[1]).rjust(30))
+
+        input("Enter para continuar")
+        reports()
+
+    elif userInput == 9:
+
+        cabecera = (" APUESTA MEDIA PRIMERA RONDA ".center(60, "=") + "\n" + "ID Partida".ljust(
+            20) + "Apuesta Media Primera Ronda".rjust(30) +
+                    "\n" + "*".center(60, "="))
+
+        cursor = BBDDconnect()
+        cursor.execute("SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));")
+        cursor.execute("select * from informe_2")
+        results = cursor.fetchall()
+        print(cabecera)
+        for row in results:
+            print(str(row[0]).ljust(20) + str(row[1]).rjust(30))
+
+        input("Enter para continuar")
+        reports()
+
+    elif userInput == 10:
+
+        cabecera = (" APUESTA MEDIA ÚLTIMA RONDA ".center(60, "=") + "\n" + "ID Partida".ljust(20) +
+                    "Apuesta Media Última Ronda".rjust(30) + "\n" + "*".center(60, "="))
+
+        cursor = BBDDconnect()
+        cursor.execute("SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));")
+        cursor.execute("select * from informe_2")
+        results = cursor.fetchall()
+        print(cabecera)
+        for row in results:
+            print(str(row[0]).ljust(20) + str(row[1]).rjust(30))
+
+        input("Enter para continuar")
+        reports()
+
+    elif userInput == 11:
+        pass
+        # mainMenu()
+
 def reports():
     '''Función que nos muestra el menú de reportes, y una vez elegida una opción, el reporte
 correspondiente'''
